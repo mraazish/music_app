b933af0287749e3c60e9012adcac500ac9c41a7a
mraazish <117164128+mraazish@users.noreply.github.com>
2023-12-14 21:39:32 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainPage from "./Pages/MainPage/MainPage";
+import style from "./global.module.scss";
+import Playbar from "./components/Playbar/Playbar";
+
+const App = () => (
+  <div className={style.wrapper}>
+    <MainPage />
+    <Playbar/>
+  </div>
+);
+export default App;
